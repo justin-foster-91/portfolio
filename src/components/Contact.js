b3279b3bb10ace67d5ad7ccd4e77e5418e27bf4b
justin-foster-91 <jfoster91@gmail.com>
2024-08-05 11:17:20 -0700

    Changed form control

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
-
 import Send from "@material-ui/icons/Send";
+import { FormControl } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   contactContainer: {
@@ -61,44 +61,54 @@ const InputField = withStyles({
   },
 })(TextField);
 
+/*
+<form action="https://formspree.io/f/{form_id}" method="post">
+  <label for="email">Your Email</label>
+  <input name="Email" id="email" type="email">
+  <button type="submit">Submit</button>
+</form>
+*/
+
 const Contact = () => {
   const classes = useStyles();
   return (
     <Box component="div" className={classes.contactContainer}>
       <Grid container justify="center">
-        <Box component="form" className={classes.form} action="https://formspree.io/jfoster91@gmail.com" method="post" >
-          <Typography variant="h5" className={classes.heading}>
-            Hire or Contact me...
-          </Typography>
-          <InputField
-            fullWidth={true}
-            label="Name"
-            variant="outlined"
-            inputProps={{ className: classes.input }}
-          />
-          <InputField
-            fullWidth={true}
-            label="Email"
-            variant="outlined"
-            inputProps={{ className: classes.input }}
-            className={classes.field}
-          />
-          <InputField
-            fullWidth={true}
-            label="Message"
-            variant="outlined"
-            multiline
-            rows={4}
-            inputProps={{ className: classes.input }}
-          />
-          <Button
-            variant="outlined"
-            fullWidth={true}
-            endIcon={<Send />}
-            className={classes.button}
-          >
-            Contact Me
-          </Button>
+        <Box component="form" className={classes.form}>
+          <FormControl action="https://formspree.io/jfoster91@gmail.com" method="post">
+            <Typography variant="h5" className={classes.heading}>
+              Hire or Contact me...
+            </Typography>
+            <InputField
+              fullWidth={true}
+              label="Name"
+              variant="outlined"
+              inputProps={{ className: classes.input }}
+            />
+            <InputField
+              fullWidth={true}
+              label="Email"
+              variant="outlined"
+              inputProps={{ className: classes.input }}
+              className={classes.field}
+            />
+            <InputField
+              fullWidth={true}
+              label="Message"
+              variant="outlined"
+              multiline
+              rows={4}
+              inputProps={{ className: classes.input }}
+            />
+            <Button
+              variant="outlined"
+              fullWidth={true}
+              endIcon={<Send />}
+              className={classes.button}
+            >
+              Contact Me
+            </Button>
+          </FormControl>
         </Box>
       </Grid>
     </Box>
