b87327902aca6f1e94b3f86fe8e1d6fbd67bcedf
justin-foster-91 <jfoster91@gmail.com>
2024-08-11 22:41:08 -0700

    About to attempt Backdrop implementation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ import ContactMail from "@material-ui/icons/ContactMail";
 import { makeStyles } from "@material-ui/core/styles";
 import self from "../images/me.jpeg";
 import Footer from "../components/Footer";
-
+// import Backdrop from '@mui/material/Backdrop';
 
 const menuItems = [
   { listIcon: <Home />, listText: "Home", listPath: "/" },
@@ -30,12 +30,16 @@ const menuItems = [
 ];
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
-
   const classes = useStyles();
 
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
   const sideList = () => (
-    <Box className={classes.menuSliderContainer} component="div">
+    <Box className={classes.menuSliderContainer} component="div"  onClick={toggleDrawer(false)}>
       <Avatar className={classes.avatar} src={self} alt="Justin Foster" />
       <Divider />
       <List>
@@ -60,10 +64,10 @@ const Navbar = () => {
 
   return (
     <>
-      <Box component="nav">
+      <Box component="nav" >
         <AppBar position="static" className={classes.appbar}>
           <Toolbar className={open && classes.toolHeader}>
-            <IconButton onClick={() => setOpen(true)}>
+            <IconButton onClick={toggleDrawer(true)}>
               <ArrowBack className={classes.arrow} />
             </IconButton>
             <Typography variant="h5" className={classes.title}>
@@ -72,10 +76,16 @@ const Navbar = () => {
           </Toolbar>
         </AppBar>
       </Box>
-      <Drawer open={open} anchor="left" hideBackdrop="true" onClose={() => setOpen(false)}>
-        {sideList()}
-        <Footer />
-      </Drawer>
+      {/* <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={toggleDrawer(false)}
+      > */}
+        <Drawer open={open} anchor="left" hideBackdrop="true" onClose={toggleDrawer(false)}>
+          {sideList()}
+          <Footer />
+        </Drawer>
+      {/* </Backdrop> */}
     </>
   );
 };
